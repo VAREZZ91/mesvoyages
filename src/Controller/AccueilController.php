65c2fd8e23a240dbbea3fec6066e73ba8d8d7a44
zarae <zarae@PC-Mayrbek>
2024-12-18 21:42:39 +0100

    Phase 1 : création du projet. Phase 2 : premier test "Hello World!"

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of AccueilController
+ *
+ * @author zarae
+ */
+class AccueilController {
+    
+    #[Route('/', name: 'accueil')]
+    public function index(): Response{ 
+        return new Response('Hello world');
+    
+    }
+    
+}
